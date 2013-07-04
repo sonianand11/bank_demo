96cd73cf9c072b78ddfc00f9330e478f6976a677
anand <anand@anand-laptop.(none)>
2013-07-04 12:51:23 +0530

    intial commit to learn git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BankDemo::Application.config.secret_token = '4eb0c89fba186911474c3699e2656a8141846a98be3d8d57ede794f6d0504bd30898a90eeacd6cc4195112d5ac07feb3d76140ffcc9bd78ce273ab63b278d19a'
